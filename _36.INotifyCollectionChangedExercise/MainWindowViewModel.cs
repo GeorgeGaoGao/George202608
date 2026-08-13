03596108b267e165b46e91e4438b2221570767ce
George <georgeeasycfg@163.com>
2026-08-13 21:31:28 +0800

    20260813 noon

---
+++ _36.INotifyCollectionChangedExercise/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GeorgeWpfDLL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace _36.INotifyCollectionChangedExercise
+{
+    public class MainWindowViewModel:ObservableObject
+    {
+        public Person CurrentPerson { get; set; }
+        public ObservableCollection<Person> People { get; set; }=new ObservableCollection<Person>();
+        public MainWindowViewModel()
+        {
+            
+        }
+    }
+}
---
+++ _36.INotifyCollectionChangedExercise/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace _36.INotifyCollectionChangedExercise
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}

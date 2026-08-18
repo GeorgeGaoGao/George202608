18eff81ad88d3e63c1d6b7cb90fc014c770293d7
George <georgeeasycfg@163.com>
2026-08-18 21:34:47 +0800

    20260818 night

---
+++ _49.TemplateBindingExercise/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _49.TemplateBindingExercise
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ _49.TemplateBindingExercise/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GeorgeWpfDLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _49.TemplateBindingExercise
+{
+    public class MainWindowViewModel:ObservableObject
+    {
+        public Person CurrentPerson { get; set; }
+    }
+}

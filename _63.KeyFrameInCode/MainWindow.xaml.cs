d08949f0c73a6240fa2aaf9234d67ef19776a9cf
George <georgeeasycfg@163.com>
2026-08-27 21:30:44 +0800

    20260827 afternoon

---
+++ _63.KeyFrameInCode/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _63.KeyFrameInCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void myBorder_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            LinearGradientBrush brush=myBorder.Background as LinearGradientBrush;
+            PointAnimationUsingKeyFrames startPointAnimation = new PointAnimationUsingKeyFrames();
+            PointAnimationUsingKeyFrames endPointAnimation = new PointAnimationUsingKeyFrames();
+
+            Random random=new Random();
+            double x=random.NextDouble();
+            Thread.Sleep(1);
+            double y=random.NextDouble();
+            PointKeyFrame startKeyFrame = new LinearPointKeyFrame(new Point(x, y), new TimeSpan(0, 0, 2));
+            startPointAnimation.KeyFrames.Add(startKeyFrame);
+
+            x=random.NextDouble();
+            Thread.Sleep(1);
+            y=random.NextDouble();
+            PointKeyFrame endKeyFrame = new LinearPointKeyFrame(new Point(x, y), new TimeSpan(0, 0, 3));
+            startPointAnimation.KeyFrames.Add(endKeyFrame);
+
+            brush.BeginAnimation(LinearGradientBrush.StartPointProperty, startPointAnimation);
+            brush.BeginAnimation(LinearGradientBrush.EndPointProperty, endPointAnimation);
+        }
+    }
+}
